ef899756fe1be0a0e425d4bfa788b0a2437ce895
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-12-22 19:56:41 +0300

    merge conflict

--- src/test.c
+++ src/test.c
@@ -27,6 +27,9 @@
 #include "car.h"
 #include "dev/trimpot.h"
 #include "dev/joystick.h"
+#include "dev/uart.h"
+#include "dev/timer.h"
+#include "dev/esp8266.h"
 
 #include "Ultrasonic.h"
 
@@ -278,10 +281,73 @@ void (*func_list[])(void) = {
 		test_joystick
 };
 
+void uart_buffer_callback_for_test(char *recString) {
+	uart_context_t *context = uart_getUartContext(0);
+	
+	uart_send(context, *recString);
+}
+
+void test_uart() {
+	uart_context_t *context = uart_getUartContext(0);
+	uart_setBaudRate(context, 9600);
+	uart_init(context);
+	
+	uart_setBufferCompleteCallback(context, uart_buffer_callback_for_test);
+	
+	uart_sendString(context, "Hello world!", 12);
+	
+}
+
+int str_length(char *str) {
+	int i = 0;
+	while(str[i++] != 0);
+	return i;
+}
+
+void on_esp_response(char *response) {
+	uart_context_t *context = uart_getUartContext(0);
+	
+	uart_sendString(context, response, str_length(response));
+}
+
+void uart_esp_command(char *recString) {
+	static uint8_t i = 0;
+	static char buffer[256];
+
+	uart_context_t *context = uart_getUartContext(0);
+		
+	buffer[i++] = *recString;
+	if(*recString == '\r') {
+		buffer[i] = '\n';
+		buffer[i + 1] = 0;
+		esp8266_send(esp8266_get_context(), buffer, i + 1);
+		i = 0;
+		uart_send(context, '\n');
+	}
+	
+	uart_send(context, *recString);
+}
+
+void test_esp() {
+	uart_context_t *uart_context = uart_getUartContext(0);
+	uart_setBaudRate(uart_context, 9600);
+	uart_init(uart_context);
+	
+	uart_setBufferCompleteCallback(uart_context, uart_esp_command);
+	
+	esp_context_t *context = esp8266_get_context();
+	
+	esp8266_init(context);
+	
+	esp8266_reset(context);
+	
+	esp8266_setOnResponseListener(context, on_esp_response);
+}
+
 int main(void) {
 
 #if defined (__USE_LPCOPEN)
-	// Read clock settings and update SystemCoreClock variable
+    // Read clock settings and update SystemCoreClock variable
 	SystemCoreClockUpdate();
 #if !defined(NO_BOARD_LIB)
 	// Set up and initialize all required blocks and
@@ -301,7 +367,10 @@ int main(void) {
 	//    rgb_led->commands[RGB_LED_COMMAND_SET_GREEN_VALUE].execute(0);
 	//    rgb_led->commands[RGB_LED_COMMAND_SET_BLUE_VALUE].execute(0);
 
-	func_list[run_config]();
+	// func_list[run_config]();
+
+    test_esp();
+    
 
 	// Force the counter to be placed into memory
 	volatile static int i = 0 ;
